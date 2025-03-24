aef247f69950acd9ae29f2d06817bd37fbd4473d
SukhmanAulakh18 <ausukhma@sheridancollege.ca>
2025-03-24 06:16:32 -0400

    added api routes

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -52,11 +52,16 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc
 
-    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+    res.cookie("access_token", token, { 
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true
+    }).status(200).json({
       success: true,
       message: "Login Successful!",
       rest,
     })
+    
   } catch (error) {
     next(error)
   }
